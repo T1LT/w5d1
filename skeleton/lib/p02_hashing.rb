1b6e042668b0ee8b734a5d11deb3d151fe8b4226
Nishant Racherla <nishant.racherla@gmail.com>
2022-10-31 15:20:29 -0700

    finished phase 2

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    res = 0 
+    (0...self.length).each do |i|
+      res += (i * self[i])
+    end
+    res.hash
   end
 end
 
 class String
   def hash
+    res = 0
+    (0...self.length).each do |i|
+      index = self[i].ord
+      res += (i * index)
+    end
+    res.hash
   end
 end
 
@@ -16,6 +27,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort.to_s.hash
   end
 end
